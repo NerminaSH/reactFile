61fcb528d1aecd132b89748f67d2020ee22f437a
Nermin Shenouda <nerminawessam2@gmail.com>
2023-08-17 22:50:39 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
+import {Routes, Route, Link } from "react-router-dom";
+import Home from './pages/home';
+import Css from './pages/css';
+import Html from './pages/html';
+import JavaScript from './pages/javascript';
 
+
+function App() {
+return(
+  <div className="App">
+    <h1 style={{ color: "red" }}>Welcome to My Page</h1>
+    <Routes>
+      <route path="/" element={<Home/>}/>
+      <route path="/html" element={<Html/>}/>
+      <route path="/css" element={<Css/>}/>
+      <route path="/JavaScript" element={<JavaScript/>}/>
+    </Routes>
+  </div>
+)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } 
 from "react-router-dom";
+import App from './App'
 import Home from './pages/home'
 import Html from './pages/html'
 import Css from './pages/css'
